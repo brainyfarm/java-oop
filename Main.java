a26e4aab04aea4e7708f95c0bfaaf0229440b20c
Olawale Akinseye <brainyfarm@gmail.com>
2018-05-23 12:19:13 +0100

    add basic code

---
+++ Main.java
@@ -0,0 +1,9 @@
+import greet.greeting;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Java is easy");
+        greeting.greet("Olawale");
+    }
+
+}
---
+++ greet/greeting.java
@@ -0,0 +1,7 @@
+package greet;
+
+public class greeting {
+    public static void greet(String firstname) {
+        System.out.println(firstname);
+    }
+}
