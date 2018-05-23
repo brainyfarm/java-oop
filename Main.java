f243b928d3e8bf91dd28984fef446d2b4f96d5a9
Olawale Akinseye <brainyfarm@gmail.com>
2018-05-23 13:05:55 +0100

    add arrays

--- Main.java
+++ Main.java
@@ -2,6 +2,10 @@ import greet.greeting;
 
 public class Main {
     public static void main(String[] args) {
+        // Array of 10 numbers and string with defined lengths
+        int[] numbers = new int[10];
+        String[] names = new String[15];
+
         System.out.println("Java is easy");
         greeting.greet("Olawale");
     }
